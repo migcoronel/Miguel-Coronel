e2392e298a9560dbe00e810f3a9565318eec1227
migcoronel <mcoronel64@gmail.com>
2022-04-26 14:23:15 -0400

    creating the project

---
+++ whatsapp-group-api/src/main/java/com/example/whatsappgroupapi/WhatsappGroupApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatsappgroupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappGroupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappGroupApiApplication.class, args);
+	}
+
+}
---
+++ whatsapp-group-api/src/test/java/com/example/whatsappgroupapi/WhatsappGroupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsappgroupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappGroupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
